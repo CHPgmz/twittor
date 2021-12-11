8f4cad41eb9023119efce2849b36c7c184041dba
Jose Alfredo <chpgomezito@gmail.com>
2021-12-10 22:24:16 -0600

    Se cambiaron los estilos

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v2';
+const STATIC_CACHE = 'static-v4';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -41,6 +41,11 @@ self.addEventListener('activate', e => {
             if (key != STATIC_CACHE && key.includes('static')) {
                 return caches.delete(key);
             }
+            if (key != DYNAMIC_CACHE && key.includes('dynamic')) {
+                return caches.delete(key);
+            }
+            
+
         });
 
     });
